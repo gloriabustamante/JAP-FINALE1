7d0b9feb599bd1d1fc0feef62c6a1a6c819bd4c9
bentancorbelen <bentancorbelen2003@gmail.com>
2024-08-24 10:03:51 -0300

    correcciones y cambios de estilo al boton, validacion campos

--- js/login.js
+++ js/login.js
@@ -1,14 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let   username = document.querySelector("#username");
+    let password = document.querySelector("#password");
+    let buttonLogin = document.querySelector("#buttonLogin");
 
-function login() {
-
-    let username = document.querySelector("#username").value;
-    let password = document.querySelector("#password").value;
-
-    if (username != null && password != null) {
-        localStorage.setItem("username", username);
-        window.location.href = "index.html";
-    } else {
-        alert("Error");
+    function alternarbotones() {
+        if (username.value.trim() !== '' && password.value.trim() !== '') {
+            buttonLogin.disabled = false; 
+            buttonLogin.classList.remove('buttonDisabled'); 
+            buttonLogin.classList.add('button');
+            localStorage.setItem("username", username)
+        } else {
+            buttonLogin.disabled = true; 
+            buttonLogin.classList.remove('button'); 
+            buttonLogin.classList.add('buttonDisabled'); 
+        }
     }
-};
 
+    username.addEventListener('input', alternarbotones);
+    password.addEventListener('input', alternarbotones);
+
+    alternarbotones();
+});
