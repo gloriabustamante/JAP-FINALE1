094d4f3feafb75277aad9bcdeae0aa4584797bdb
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2024-08-31 14:47:29 -0300

    cambios en la nav bar

--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
+    function displayNavBarName() {
+        let navListItem = document.querySelector('#navName');
+        let navUsername = localStorage.getItem("username");
+       
+    if (navUsername) {
+            let navLink = document.createElement('a');
+            navLink.textContent = navUsername;
+            navListItem.appendChild(navLink);
+            navLink.setAttribute('class', 'nav-link')
+            navLink.setAttribute('href', 'my-profile.html')
+        }
+    }
+
+    function displayLogOut() {
+        let navListItem = document.querySelector('#logout');
+        let navUsername = localStorage.getItem("username");
+       
+    if (navUsername) {
+            let navLink = document.createElement('a');
+            navLink.textContent = 'Cerrar Sesión';
+            navListItem.appendChild(navLink);
+            navLink.setAttribute('class', 'nav-link')
+            navLink.setAttribute('href', 'login.html')
+        }
+    }
+    displayNavBarName()
+    displayLogOut()
 });
 
 
