834ea782121827cdd10b415f8dc85ea366381f48
LuciaValverdeCP <lvalverde.lez@gmail.com>
2024-12-01 19:58:58 -0300

    Desafiate

--- src/app.js
+++ src/app.js
@@ -1,56 +1,57 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+  const express = require('express');
+  const cors = require('cors');
+  const jwt = require('jsonwebtoken');
+  const bodyParser = require('body-parser');
+  const app = express();
+  const port = 3000;
 
-const SECRET_KEY = "tu_clave_secreta";
+  const SECRET_KEY = "tu_clave_secreta";
 
-const productsRoutes = require('./routes/products');
-const catProductRoutes = require('./routes/cats_products');
-const categoryRoutes = require('./routes/cats');
-const commentsRoutes = require('./routes/products_comments');
+  const productsRoutes = require('./routes/products');
+  const catProductRoutes = require('./routes/cats_products');
+  const categoryRoutes = require('./routes/cats');
+  const commentsRoutes = require('./routes/products_comments');
+  const cartRoutes = require('./routes/cart');
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
+  app.use(cors());
+  app.use(express.json());
+  app.use(bodyParser.json());
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (username === "admin" && password === "admin") {
-    const token = jwt.sign({ username }, SECRET_KEY);
-    res.status(200).json({ token });
-  } else {
-    res.status(401).json({ message: "Usuario y/o contraseña incorrecto" });
-  }
-});
+  app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    if (username === "admin" && password === "admin") {
+      const token = jwt.sign({ username }, SECRET_KEY);
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ message: "Usuario y/o contraseña incorrecto" });
+    }
+  });
 
-// Middleware que autoriza a realizar peticiones
+  // Middleware que autoriza a realizar peticiones
 
-const authenticateToken = (req, res, next) => {
-  try {
-    const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
-    console.log(decoded);
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Usuario no autorizado" });
-  }
-};
+  const authenticateToken = (req, res, next) => {
+    try {
+      const decoded = jwt.verify(req.headers["access-token"], SECRET_KEY);
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Usuario no autorizado" });
+    }
+  };
 
-// Rutas protegidas
-app.use('/api/products', authenticateToken, productsRoutes);
-app.use('/api/products/:id', authenticateToken, productsRoutes);
-app.use('/api/categories', authenticateToken, categoryRoutes);
-app.use('/api/comments', authenticateToken, commentsRoutes);
-app.use('/api/comments/:id', authenticateToken, commentsRoutes);
-app.use('/api/cats_products', authenticateToken, catProductRoutes);
-app.use('/api/cats_products/:id', authenticateToken, catProductRoutes);
+  // Rutas protegidas
+  app.use('/api/products', authenticateToken, productsRoutes);
+  app.use('/api/products/:id', authenticateToken, productsRoutes);
+  app.use('/api/categories', authenticateToken, categoryRoutes);
+  app.use('/api/comments', authenticateToken, commentsRoutes);
+  app.use('/api/comments/:id', authenticateToken, commentsRoutes);
+  app.use('/api/cats_products', authenticateToken, catProductRoutes);
+  app.use('/api/cats_products/:id', authenticateToken, catProductRoutes);
+  app.use('/api/cart', cartRoutes);
 
-app.get("/protected", authenticateToken, (req, res) => {
-  res.status(200).json({ message: "Acceso permitido", user: req.user });
-});
+  app.get("/protected", authenticateToken, (req, res) => {
+    res.status(200).json({ message: "Acceso permitido", user: req.user });
+  });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
